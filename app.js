55be7ee48c2dfb6d7f6baab293d025f195ef1f73
kimicasamina <kimicasamina@gmail.com>
2023-01-13 17:43:56 +0800

    added a reset button that will reset the timer back to 0

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const startBtn = document.querySelector('.start')
 const pauseBtn = document.querySelector('.pause')
 const resetBtn = document.querySelector('.reset')
+const minText = document.querySelector('#min')
+const secText = document.querySelector('#sec')
+let timer;
 
 let sec = 0
 let min = 0
@@ -10,14 +13,15 @@ let isRunning = false
 
 startBtn.addEventListener('click', startTimer)
 pauseBtn.addEventListener('click', pauseTimer)
+resetBtn.addEventListener('click', resetTimer)
 function startTimer(){
   timer = setInterval(() => {
     sec++
     console.log(sec)
-    if (sec => 60){
+    if (sec >= 60){
       sec = 0
     }
-    if (min => 25){
+    if (min >= 25){
       min = 0
     }
     updateText()
@@ -28,9 +32,13 @@ function pauseTimer(){
   clearInterval(timer)
 }
 
+function resetTimer(){
+  sec = 0
+  min = 0
+  updateText()
+}
+
 function updateText(){
-  const minText = document.querySelector('#min')
-  const secText = document.querySelector('#sec')
   minText.innerText = min
   secText.innerText = sec 
 }
