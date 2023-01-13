a0d28e2cbb69fc4709da3a7b655ade7fa252f2a7
kimicasamina <kimicasamina@gmail.com>
2023-01-13 19:41:59 +0800

    added font styles

---
+++ README.md
@@ -0,0 +1,15 @@
+# POMODORO TIMER
+A simple pomodoro timer that has a 25 minutes interval. 
+For every pomodoro you finished, you'll gain a tomato. 
+The user can pause and reset the timer. 
+
+[live preview](https://kimicasamina.github.io/pomodoro-timer/)
+
+## SCREENSHOTS
+![screenshot](https://user-images.githubusercontent.com/122260532/211793892-81bcbb9f-9172-48a6-bd01-19db4330ba2b.png)
+
+## TECHNOLOGIES USED:
+- HTML
+- CSS
+- SASS
+- JAVASCRIPT
--- app.js
+++ app.js
@@ -1,57 +1,55 @@
-const startBtn = document.querySelector('.start')
-const pauseBtn = document.querySelector('.pause')
-const resetBtn = document.querySelector('.reset')
-const tomatoCountText = document.querySelector('#tomato-count')
-const minText = document.querySelector('#min')
-const secText = document.querySelector('#sec')
-const tomatoes = document.querySelector('.tomatoes')
+const startBtn = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+const resetBtn = document.querySelector(".reset");
+const tomatoCountText = document.querySelector("#tomato-count");
+const minText = document.querySelector("#min");
+const secText = document.querySelector("#sec");
+const tomatoes = document.querySelector(".tomatoes");
 
 let timer;
-let sec = 0
-let min = 0
-let tomatoCount = 0
+let sec = 0;
+let min = 1;
+let tomatoCount = 0;
 
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
 
-startBtn.addEventListener('click', startTimer)
-pauseBtn.addEventListener('click', pauseTimer)
-resetBtn.addEventListener('click', resetTimer)
-
-function startTimer(){
+function startTimer() {
   timer = setInterval(() => {
-    sec++
-    console.log(sec)
-    if (sec >= 60){
-      min++
-      sec = 0
+    sec++;
+    console.log(sec);
+    if (sec >= 60) {
+      min++;
+      sec = 0;
     }
-    if (min >= 25){
-      min = 0
-      addTomato()
+    if (min >= 2) {
+      min = 0;
+      addTomato();
     }
-    updateText()
-  }, 1000)
+    updateText();
+  }, 1000);
 }
 
-
-function addTomato(){
-    tomatoCount++
-    tomatoCountText.innerText = tomatoCount
-    const tomato = document.createElement('div')
-    tomato.classList.add('tomato-plant')
-    tomatoes.append(tomato)
+function addTomato() {
+  tomatoCount++;
+  tomatoCountText.innerText = tomatoCount;
+  const tomato = document.createElement("div");
+  tomato.classList.add("tomato-plant");
+  tomatoes.append(tomato);
 }
 
-function pauseTimer(){
-  clearInterval(timer)
+function pauseTimer() {
+  clearInterval(timer);
 }
 
-function resetTimer(){
-  sec = 0
-  min = 0
-  updateText()
+function resetTimer() {
+  sec = 0;
+  min = 0;
+  updateText();
 }
 
-function updateText(){
-  minText.innerText = min
-  secText.innerText = sec 
+function updateText() {
+  minText.innerText = min;
+  secText.innerText = sec;
 }
