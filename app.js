f6681983fc76bbcde74fe994e6525f7fd36178ce
kimicasamina <kimicasamina@gmail.com>
2023-01-13 18:37:24 +0800

    added function that will draw a tomato on the screen everytime the timer hits 25 minutes

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const startBtn = document.querySelector('.start')
 const pauseBtn = document.querySelector('.pause')
 const resetBtn = document.querySelector('.reset')
+const tomatoCountText = document.querySelector('#tomato-count')
 const minText = document.querySelector('#min')
 const secText = document.querySelector('#sec')
-let timer;
+const tomatoes = document.querySelector('.tomatoes')
 
+let timer;
 let sec = 0
 let min = 0
 let tomatoCount = 0
 
-let isRunning = false 
 
 startBtn.addEventListener('click', startTimer)
 pauseBtn.addEventListener('click', pauseTimer)
 resetBtn.addEventListener('click', resetTimer)
+
 function startTimer(){
   timer = setInterval(() => {
     sec++
     console.log(sec)
     if (sec >= 60){
+      min++
       sec = 0
     }
     if (min >= 25){
       min = 0
+      addTomato()
     }
     updateText()
   }, 1000)
+}
+
+
+function addTomato(){
+    tomatoCount++
+    tomatoCountText.innerText = tomatoCount
+    const tomato = document.createElement('div')
+    tomato.classList.add('tomato-plant')
+    tomatoes.append(tomato)
 }
 
 function pauseTimer(){
