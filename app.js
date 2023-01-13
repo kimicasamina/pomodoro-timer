953ecc89583500cc8a6f2497944d6a2f934f277f
kimicasamina <kimicasamina@gmail.com>
2023-01-13 17:33:02 +0800

    added a pause button that will pause the timer

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ let tomatoCount = 0
 let isRunning = false 
 
 startBtn.addEventListener('click', startTimer)
-
+pauseBtn.addEventListener('click', pauseTimer)
 function startTimer(){
   timer = setInterval(() => {
     sec++
     console.log(sec)
-    if (sec > 60){
+    if (sec => 60){
       sec = 0
     }
-    if (min > 25){
+    if (min => 25){
       min = 0
     }
+    updateText()
   }, 1000)
+}
+
+function pauseTimer(){
+  clearInterval(timer)
+}
+
+function updateText(){
+  const minText = document.querySelector('#min')
+  const secText = document.querySelector('#sec')
+  minText.innerText = min
+  secText.innerText = sec 
 }
