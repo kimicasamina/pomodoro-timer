e4e5bc02090d1be7c390acad758f7196530b4fad
kimicasamina <kimicasamina@gmail.com>
2023-01-13 16:54:05 +0800

    added timer function

---
+++ app.js
@@ -0,0 +1,24 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const resetBtn = document.querySelector('.reset')
+
+let sec = 0
+let min = 0
+let tomatoCount = 0
+
+let isRunning = false 
+
+startBtn.addEventListener('click', startTimer)
+
+function startTimer(){
+  timer = setInterval(() => {
+    sec++
+    console.log(sec)
+    if (sec > 60){
+      sec = 0
+    }
+    if (min > 25){
+      min = 0
+    }
+  }, 1000)
+}
